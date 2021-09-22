dac050e91ff46be64fa1c7565c3211f5b56ec99b
arungothwal <avisingh10.as@gmail.com>
2021-09-22 14:49:23 +0530

    fourth commit

--- register/settings.py
+++ register/settings.py
@@ -156,5 +156,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'avisingh10.as@gmail.com'
-EMAIL_HOST_PASSWORD = '9810198343'
+EMAIL_HOST_USER = '##########'
+EMAIL_HOST_PASSWORD = '#########'
